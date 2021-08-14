367e50951098ff428f97c4ddada153c7a08024ed
jedCCC <a535297316@gmail.com>
2021-08-14 22:17:38 +1000

    fix typo

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const {
   addUser
-} = require('../controllers/Users')
+} = require('../controllers/users')
 
 
 router.post('/', addUser);
